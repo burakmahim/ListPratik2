48559c1a1558e0d88bf445437f2ea72185d78275
burakmahim <burakmahim718@gmail.com>
2024-11-06 11:25:18 +0300

    Proje dosyası ekle.

---
+++ ListPratik2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Kahve isimlerini tutmak için bir liste oluştur
+List<string> kahveListesi = new List<string>();
+
+// Kullanıcıdan 5 kahve ismi al ve listeye ekle
+Console.WriteLine("Lütfen 5 adet kahve ismi girin:");
+for (int i = 1; i <= 5; i++)
+{
+    Console.Write($"Kahve {i}: ");
+    string kahveIsmi = Console.ReadLine();
+    kahveListesi.Add(kahveIsmi);
+}
+
+// foreach döngüsü ile liste içerisindeki kahve isimlerini ekrana yazdır
+Console.WriteLine("\nGirdiğiniz kahve isimleri:");
+foreach (string kahve in kahveListesi)
+{
+    Console.WriteLine(kahve);
+}
